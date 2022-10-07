2d2bbb6ebbac018a0d935905b21e3ea9960ff8fb
RunyShark <spartadragon117@live.com.mx>
2022-10-07 14:09:02 -0500

    pop, popToTop

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,6 +1,7 @@
 import {StackScreenProps} from '@react-navigation/stack';
 
 export interface PropsNavigation extends StackScreenProps<any, any> {}
+
 export enum ScreenNavigation {
   Tap1Screen,
   Tap2Screen,
--- src/screen/Tap3/index.tsx
+++ src/screen/Tap3/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 import {Button, Text, View} from 'react-native';
-import {PropsNavigation, ScreenNavigation} from '../../index';
+import {PropsNavigation, styles} from '../../index';
 
 export const Tap3Screen = ({navigation}: PropsNavigation) => {
   return (
-    <View>
+    <View style={styles.globalMargin}>
       <Text>Screen3</Text>
-      <Button
-        title="Next Screen"
-        onPress={() => navigation.navigate(ScreenNavigation[0])}
-      />
+      <Button title="To return" onPress={() => navigation.pop()} />
+      <Button title="Next Screen" onPress={() => navigation.popToTop()} />
     </View>
   );
 };
